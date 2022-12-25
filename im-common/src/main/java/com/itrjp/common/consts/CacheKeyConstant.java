f6c6f76f8c262d6d9d91090248045251319b7f4b
jianpeng.ren <jianpeng.ren@vhall.com>
2022-12-25 23:29:20 +0800

    rename: redis key

--- im-common/src/main/java/com/itrjp/common/consts/CacheKeyConstant.java
+++ im-common/src/main/java/com/itrjp/common/consts/CacheKeyConstant.java
@@ -2,6 +2,8 @@ package com.itrjp.common.consts;
 
 /**
  * 缓存key常量
+ * <p>
+ * TODO key 格式: 服务名称:模块名称:业务名称[:业务id]
  *
  * @author <a href="mailto:r979668507@gmail.com">renjp</a>
  * @date 2022/7/26 11:52
@@ -12,17 +14,31 @@ public final class CacheKeyConstant {
      * <p>
      * 使用zset
      */
-    public static final String CONNECT_NODE_LOAD = "im:stat:connect-node:load";
+    public static final String CONNECT_NODE_LOAD = "im:connect-node:load";
+    public static final String IM_STAT_CHANNEL_PV = "im:stat:channel:pv:";
+
     /**
      * connect 服务 节点负载.
      */
-    public static final String CONNECT_NODE = "im:stat:connect-node:load:";
+    public static final String CONNECT_NODE = "im:connect-node:load:";
 
     /**
      * 所有connect 节点信息
      * <p>
      * 使用hash
      */
-    public static final String CONNECT_NODE_INFO = "im:stat:connect-node:info";
+    public static final String CONNECT_NODE_INFO = "im:connect-node:info";
 
+    /**
+     * <pre>
+     * connect 节点与频道映射关系(hash)
+     * key: connectNodeId
+     * value: channelId
+     * </pre>
+     */
+    public static final String CONNECT_NODE_CHANNEL_MAPPING = "im:connect-node:channel:mapping";
+    /**
+     * 频道用户列表
+     */
+    public static final String CHANNEL_USER = "im:channel:";
 }
--- im-stat/src/main/java/com/itrjp/im/stat/service/impl/ChannelStatServiceImpl.java
+++ im-stat/src/main/java/com/itrjp/im/stat/service/impl/ChannelStatServiceImpl.java
@@ -2,10 +2,13 @@ package com.itrjp.im.stat.service.impl;
 
 import com.itrjp.im.stat.pojo.ChannelInfo;
 import com.itrjp.im.stat.service.ChannelStatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static com.itrjp.common.consts.CacheKeyConstant.IM_STAT_CHANNEL_PV;
 
 /**
  * TODO
@@ -14,22 +17,19 @@ import java.util.Optional;
  * @date 2022/7/24 18:12
  */
 @Service
+@RequiredArgsConstructor
 public class ChannelStatServiceImpl implements ChannelStatService {
     private final RedisTemplate<String, String> redisTemplate;
-
-    public ChannelStatServiceImpl(RedisTemplate<String, String> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     @Override
     public void online(String channelId, String sessionId, String userName) {
 
-        redisTemplate.opsForValue().increment("im:stat:channel:pv:" + channelId);
+        redisTemplate.opsForValue().increment(IM_STAT_CHANNEL_PV + channelId);
     }
 
     @Override
     public void offline(String channelId, String sessionId, String userName) {
-        redisTemplate.opsForValue().decrement("im:stat:channel:pv:" + channelId);
+        redisTemplate.opsForValue().decrement(IM_STAT_CHANNEL_PV + channelId);
     }
 
 
@@ -40,7 +40,7 @@ public class ChannelStatServiceImpl implements ChannelStatService {
 
     @Override
     public Optional<Integer> getChannelConnectCount(String channelId) {
-        String count = redisTemplate.opsForValue().get("im:stat:channel:pv:" + channelId);
+        String count = redisTemplate.opsForValue().get(IM_STAT_CHANNEL_PV + channelId);
         return Optional.of(count == null ? 0 : Integer.parseInt(count));
     }
 
